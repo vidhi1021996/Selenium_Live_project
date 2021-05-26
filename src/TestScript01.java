7df4702bd27af4e259948c376ef97d661c53a8cc
Vidhi Apurva Patel <vidhi1021996@gmail.com>
2021-05-26 12:15:23 -0400

    Update TestScript01.java

--- src/TestScript01.java
+++ src/TestScript01.java
@@ -13,8 +13,7 @@ public class TestScript01 {
     public static void main(String[] args) throws Exception {	  
 	
     	//Setup Chrome driver    	
-    	System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
-    	WebDriver driver = new FirefoxDriver();
+       	WebDriver driver = new FirefoxDriver();
     	String baseUrl = "http://www.demo.guru99.com/V4/";
     	
     	// launch Firefox and direct it to the Base URL
